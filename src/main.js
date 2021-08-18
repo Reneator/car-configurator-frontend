10b9d1502718178b0d81ca6e8a3105c67cff4a80
Reneator <reneator@googlemail.com>
2021-08-18 10:57:05 +0200

    added "insecure" to html

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const app = createApp(App)
 app.use(VueAxios, axios)
 app.mount('#app')
