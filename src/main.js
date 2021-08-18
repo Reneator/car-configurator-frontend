c5774915d40032d8de40595ceecc341214c70d61
Reneator <reneator@googlemail.com>
2021-08-18 10:54:32 +0200

    added allowing unsecured connection

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const app = createApp(App)
 app.use(VueAxios, axios)
 app.mount('#app')
