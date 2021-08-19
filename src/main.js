a3140cc8a7ca42be53736457028c4a9d17d22063
Reneator <reneator@googlemail.com>
2021-08-19 14:46:54 +0200

    Removed dropdown dependency

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import Dropdown from 'vue-simple-search-dropdown';
 
 
 const app = createApp(App)
-app.use(Dropdown)
 app.use(VueAxios, axios)
 app.mount('#app')
