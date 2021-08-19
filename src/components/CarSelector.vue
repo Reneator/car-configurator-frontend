0b5ee2fc3bcd483bc33db0373429a7d32c8e7403
Reneator <reneator@googlemail.com>
2021-08-19 14:46:05 +0200

    Added select to carSelector

--- src/components/CarSelector.vue
+++ src/components/CarSelector.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="CarSelector">
+    <!--
     CarSelector Car selections:
     <button @click="getList">get api data</button>
-    <div>{{ selector_data }}</div>
+    <div>{{ selector_data }}</div>-->
+    <div>
+      <label for="cars">Choose a car:</label>
+
+      <select v-model="selectedCar">
+        <option v-for="car in response_data" :key="car.name" v-bind:value="car">{{ car.name }}</option>
+      </select>
+    </div>
+      Selected Car:
+      {{ selectedCar }}
   </div>
 </template>
 
@@ -13,6 +23,7 @@ export default {
       api: "http://157.90.116.81:8081/carconfigurator/configs/",
       response_data: "",
       selector_data: [],
+      selectedCar: "",
     };
   },
   props: {
@@ -21,18 +32,19 @@ export default {
   created() {
     this.axios.get(this.api + "all").then((response) => {
       this.response_data = response.data;
-        this.response_data.forEach((element) => {
+      this.response_data.forEach((element) => {
         this.selector_data.push(element.name);
       });
-      
     });
+    this.initialized = true;
   },
   methods: {
     getList() {
       var array = [];
       this.response_data.forEach((element) => {
         console.log(element.name);
-        array.push(element.name);})
+        array.push(element.name);
+      });
     },
   },
 };
